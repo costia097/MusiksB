04dae360ac3b32ecd92bb51a21c7e3828890d0a8
Kostia Yeriomin <adaw36909@gmail.com>
2018-12-16 14:05:17 +0200

    fixed untracked files

--- out/production/resources/db/migration/V1_1__init_data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO role (name) VALUES ('ROLE_USER');
-INSERT INTO role (name) VALUES ('ROLE_INACTIVE');
-
-INSERT INTO permission (name) VALUES ('watch');
-INSERT INTO permission (name) VALUES ('read');
-INSERT INTO permission (name) VALUES ('write');
